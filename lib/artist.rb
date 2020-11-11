55329611de7f43f3bcd4b68c0830121f7f4a3b2b
Devin Wilson <craigvin84@yahoo.com>
2020-11-11 23:08:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,69 @@
-class Artist
 
-  attr_accessor :name
+require 'pry'
 
-@@song_count = 0
+# class Artist
+# 
+#   attr_accessor :name
+# 
+# @@song_count = 0
+# 
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#   end
+# 
+#   def add_song(song)
+#     @songs << song
+#     song.artist = self
+#     @@song_count +=1
+# end
+# 
+# def add_song_by_name(name)
+#   song = Song.new(name)
+#   @songs << song
+#   song.artist = self
+#   @@song_count +=1
+# end
+# 
+# def songs
+#   @songs
+# end
+# 
+# def self.song_count
+#   @@song_count
+# end
+# 
+# 
+# end
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+class Artist	
+  attr_accessor :name	
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count +=1
-end
+  @@all = []	
 
-def add_song_by_name(name)
-  song = Song.new(name)
-  @songs << song
-  song.artist = self
-  @@song_count +=1
-end
+  def initialize(name)	
+    @name = name	
+    @@all << self	
+  end	
 
-def songs
-  @songs
-end
+  def self.all	
+    @@all	
+  end	
 
-def self.song_count
-  @@song_count
-end
+  def add_song(song)	
+    song.artist = self	
+  end	
 
+  def add_song_by_name(name)	
+    song = Song.new(name)	
+    song.artist = self	
+  end	
 
+  def songs	
+    Song.all.select {|song| song.artist == self}	
+  end	
+
+  def self.song_count	
+    Song.all.count	
+  end	
 end
