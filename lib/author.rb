150b4acc6264226a7efaa3b9121dc50777e8d489
Devin Wilson <craigvin84@yahoo.com>
2020-11-11 23:57:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,32 @@
 class Author
-
-  attr_accessor :name
+  
+  @@all_posts = []
 
   def initialize(name)
     @name = name
+    @@all_posts << self
   end
+
+  def self.all
+    @@all_posts
+  end
+
+  def add_post(post)
+    song.artist = self
+  end
+
+  def add_post_by_title(name)
+    post = post.new(name)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def self.post_count
+    Pong.all.count
+  end
+end
 
 end
