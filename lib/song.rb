e8590f85417eba6a84e0a712e21ed8d23ed49911
Devin Wilson <craigvin84@yahoo.com>
2020-11-11 23:05:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,24 +6,37 @@ class Song
   attr_accessor :name, :artist
 
 
-@@all  = []
+# @@all  = []
+# 
+#   def initialize(name, artist=nil)
+#     @name = name
+#     @artist = artist
+#     @@all << self
+#   end
+# 
+# 
+# def artist_name
+#   if self.artist
+#     self.artist.name
+#   else
+#     nil
+#   end
+# end
+# 
+# def self.all
+#   @@all
+# end
+# end
 
-  def initialize(name, artist=nil)
-    @name = name
-    @artist = artist
-    @@all << self
-  end
-
-
-def artist_name
-  if self.artist
-    self.artist.name
-  else
-    nil
-  end
+def initialize(name)
+  @name = name
 end
 
-def self.all
-  @@all
+def artist_name
+  if @artist == nil
+    return nil
+  else
+    @artist.name
+  end
 end
 end
