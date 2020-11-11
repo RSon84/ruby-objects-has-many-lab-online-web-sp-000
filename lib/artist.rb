5fdc1ac0639825583be79d5c3780f74b5253f7c5
Devin Wilson <craigvin84@yahoo.com>
2020-11-11 23:20:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,40 +1,7 @@
 
 require 'pry'
 
-# class Artist
-#
-#   attr_accessor :name
-#
-# @@song_count = 0
-#
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#   end
-#
-#   def add_song(song)
-#     @songs << song
-#     song.artist = self
-#     @@song_count +=1
-# end
-#
-# def add_song_by_name(name)
-#   song = Song.new(name)
-#   @songs << song
-#   song.artist = self
-#   @@song_count +=1
-# end
-#
-# def songs
-#   @songs
-# end
-#
-# def self.song_count
-#   @@song_count
-# end
-#
-#
-# end
+
 
 class Artist
   attr_accessor :name
@@ -64,6 +31,6 @@ class Artist
   end
 
   def self.song_count
-    Song.all.count
+    self.all.count
   end
 end
